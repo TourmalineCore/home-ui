66567aed1ba37f96e421ca9f1a496a4932bfb2f1
Maxim Rychkov <rychkov03@yandex.ru>
2025-11-14 20:56:48 +0500

    feat: add Access-Control-Allow-Origin header and configure CORS_ORIGIN var in staging env workflow

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const path = require('path');
 const { i18n } = require('./next-i18next.config');
 
 module.exports = withReactSvg({
+  async headers() {
+    // 1. CORS headers:
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          // Specifies the origin that has access to the resource
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NODE_ENV === "production"
+              ? process.env.CORS_ORIGIN
+              : process.env.CORS_ORIGIN || "http://localhost:3000",
+          }
+        ]
+      }
+    ]
+  },
   devIndicators: false,
   i18n,
   include: path.resolve(__dirname, 'icons'),
