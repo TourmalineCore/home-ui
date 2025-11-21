c535fd17109c8bb6144c3475eb488a7b29ce7807
Maxim Rychkov <rychkov03@yandex.ru>
2025-11-21 11:13:31 +0500

    feat: add new CORS headers

--- next.config.js
+++ next.config.js
@@ -4,18 +4,70 @@ const path = require('path');
 const { i18n } = require('./next-i18next.config');
 
 module.exports = withReactSvg({
+  // https://nextjs.org/docs/pages/api-reference/config/next-config-js/headers
   async headers() {
-    // 1. CORS headers:
     return [
+      // Icons are not cached by default, so we do it manually
+      {
+        source: "/(apple-touch-icon|favicon-96x96.png|favicon.ico|favicon.svg|web-app-manifest-192x192.png|web-app-manifest-512x512.png|site.webmanifest)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable"
+          }
+        ]
+      },
+
       {
         source: '/(.*)',
         headers: [
+          // 1. CORS headers:
+
+          // Indicates whether the browser should include credentials, such as cookies or HTTP authentication, in the cross-origin request
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "false",
+          },
+
           // Specifies the origin that has access to the resource
           {
-            key: 'Access-Control-Allow-Origin',
+            key: "Access-Control-Allow-Origin",
             value: process.env.NODE_ENV === "production"
               ? process.env.CORS_ORIGIN
               : process.env.CORS_ORIGIN || "http://localhost:3000",
+          },
+
+          // Indicates how the browser should handle opening new windows and tabs in the context of cross-origin requests
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+
+
+          // 2. Security headers:
+
+          // Prevents the site from being opened in an <iframe> (protection against clickjacking)
+          {
+            key: 'X-Frame-Options',
+            value: "SAMEORIGIN",
+          },
+
+          // Prevents MIME-sniffing (e.g., ensuring HTML is not treated as JS)
+          {
+            key: 'X-Content-Type-Options',
+            value: "nosniff",
+          },
+
+          // Controls what data goes into the Referer header
+          {
+            key: 'Referrer-Policy',
+            value: "no-referrer",
+          },
+
+          // Block access to browser features and APIs
+          {
+            key: 'Permissions-Policy',
+            value: "interest-cohort=(), camera=(), microphone=(), geolocation=(), fullscreen=(), payment=(), usb=(), accelerometer=(), display-capture=(), gyroscope=(), magnetometer=(), midi=(), picture-in-picture=(self), xr-spatial-tracking=()",
           }
         ]
       }
